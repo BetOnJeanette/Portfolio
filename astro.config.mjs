b1faad3393ddbbd3e3b8c7e238c8035a717f51ba
Jeanette Crull <jeanetteCrull99@gmail.com>
2024-05-24 12:36:50 -0500

    Add alpinejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://betonjeanette.github.io/",
-    base: "/Portfolio"
+  site: "https://betonjeanette.github.io/",
+  base: "/Portfolio",
+  integrations: [alpinejs()]
 });
