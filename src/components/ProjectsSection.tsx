5dc900f3d8e43fd87d5e61a0e2f4789727eb7620
Jeanette Crull <jeanetteCrull99@gmail.com>
2024-07-02 17:15:32 -0500

    Move styling out to css where possible

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,10 +1,13 @@
-import { For, createSignal, type Ref } from "solid-js"
+import { For, createSignal, onMount, type Ref } from "solid-js"
 import {DivisibilityCalculatorCard, GodotGameCode, LanPartyAppCard, DaytaCard} from "./ProjectCards/projects"
+import "../styles/projectSection.css"
 
 function ProjectSection(){
     const projectCards= [LanPartyAppCard, GodotGameCode, DaytaCard, DivisibilityCalculatorCard]
     const [frontCard, setFrontCard] = createSignal(projectCards.length - 1)
-    const CARD_OFFSET = 10;
+    const CARD_OFFSET = 15;
+    
+    const [totalHeight, setTotalHeight] = createSignal(0)
 
     function MoveToNextCard(){
         // Move forward and mod back to prevent OOB
@@ -18,26 +21,34 @@ function ProjectSection(){
     function GetReorderedCards(){
         return [...projectCards.slice(frontCard() + 1), ...projectCards.slice(0, frontCard() + 1)]
     }
+    let largestCard: HTMLDivElement | undefined;
+
+    onMount(() => {
+        if (largestCard == undefined) return;
+        setTotalHeight(largestCard.offsetHeight + (CARD_OFFSET * (projectCards.length + 1)))
+    })
 
 
-    return  <div class="AlternatingCards" style={{"position": "relative"}}>
-        <button onClick={MoveToPreviousCard}>&lt</button>
-        <For each={GetReorderedCards()}>
-            { (card, index) => {
-                return <div 
-                    class="cardAnimator"
-                    style={{
-                        "transform-origin": "top-center",
-                        "position": "absolute",
-                        "top": (index() * CARD_OFFSET).toString() + "px",
-                        "z-index": index().toString()
-                    }}
-                    >
-                    {card()}
-                </div>
-            }}
-        </For>
-        <button onClick={MoveToNextCard}>&gt</button>
+    return  <div id="projects">
+        <div class="projectButtonSpace">
+            <button id="prevButton" class="projectChangeButton" onClick={MoveToPreviousCard}>&lt</button>
+        </div>
+        <div id="cards" style={{"height": totalHeight().toString() + "px"}}>
+            <For each={GetReorderedCards()}>
+                { (card, index) => {
+                    return <div class="cardAnimator" style={{
+                            "top": (index() * CARD_OFFSET).toString() + "px",
+                            "z-index": (index() - projectCards.length).toString(),
+                        }}
+                        ref={(elm) => { if (largestCard == undefined || largestCard.clientHeight < elm.clientHeight) largestCard = elm; }}>
+                        {card()}
+                    </div>
+                }}
+            </For>
+        </div>
+        <div class="projectButtonSpace">
+            <button id="nextButton" class="projectChangeButton" onClick={MoveToNextCard}>&gt</button>
+        </div>
     </div>
 }
 
